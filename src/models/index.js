c4af79ade99ec67e78aac2fb5cca57eda977646b
sh.chen <sh.chen@shinkong.com.tw>
2022-03-07 11:18:37 +0800

    create a new model named Menu

--- src/models/index.js
+++ src/models/index.js
@@ -12,5 +12,6 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 db.store = require('./store.model')(sequelize, DataTypes, Model);
+db.menu = require('./menu.model')(sequelize, DataTypes, Model);
 
 module.exports = db;
---
+++ src/models/menu.model.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes, Model) => {
+  class Menu extends Model {};
+  Menu.init({
+    menu_id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    menu_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    store_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'store', // use table name
+        key: 'store_id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Menu', 
+    // freezeTableName: true, // modelName = tableName
+    tableName: 'menu',        // Set table name in database
+    timestamps: true,         // created and updated time
+    underscored: true,        // 蛇型命名
+    comment: "Store's menu."
+  });
+  return Menu;
+}
